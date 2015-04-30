3a91940d011e1091047bb67ac14fa29b8ab2c12b
Kirit Sælensminde <k@kirit.com>
2015-04-30 18:47:04 +0700

    Start a couple of threads.

--- ASIO/simple.cpp
+++ ASIO/simple.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <thread>
 
 
 int main() {
     std::cout << "Simple test" << std::endl;
+
+    std::thread server([]() {
+    });
+
+    std::thread client([]() {
+    });
+
+    server.join();
+    client.join();
     return 0;
 }
 
