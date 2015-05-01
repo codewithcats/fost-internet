57e2e4e408f6414ed5f865fa6994f1f6a1a93736
Kirit Sælensminde <k@kirit.com>
2015-05-01 11:34:41 +0700

    Base line that doesn't really do anything yet.

---
+++ ASIO/01.cpp
@@ -0,0 +1,34 @@
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+#include <boost/asio.hpp>
+
+
+int main() {
+    std::mutex mutex;
+    std::unique_lock<std::mutex> lock(mutex);
+    std::condition_variable signal;
+
+    std::thread server([&]() {
+        std::unique_lock<std::mutex> lock(mutex);
+        boost::asio::io_service service;
+        boost::asio::ip::tcp::acceptor listener(service);
+        listener.open(boost::asio::ip::tcp::v4());
+        listener.set_option(boost::asio::socket_base::enable_connection_aborted(true));
+        listener.bind(boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 4567));
+        listener.listen();
+        signal.notify_one();
+        std::cout << "Server set up" << std::endl;
+    });
+    signal.wait(lock);
+
+    std::thread client([]() {
+    });
+
+    server.join();
+    client.join();
+    return 0;
+}
+
